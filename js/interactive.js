40e4f304ef473040a2fe91aebc3f5d96a5be1813
taggar <rvdeede@gmail.com>
2019-05-09 10:16:55 +0200

    saving snapshot to localstorage

--- js/interactive.js
+++ js/interactive.js
@@ -1,32 +1,85 @@
 (function () {
     var canvas = document.getElementById('canvas');
+    var snapshot;
+    var snapshotImage = new Image;
 
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
 
         // resize the canvas to fill browser window dynamically
-        window.addEventListener('resize', resizeCanvas, false);
+        window.addEventListener('resize', restoreCanvas, false);
+        window.addEventListener('load', restoreCanvas, false);
 
-        function resizeCanvas() {
+        function resetCanvas() {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
-
-            /**
-             * Your drawings need to be inside this function otherwise they will be reset when 
-             * you resize the browser window and the canvas goes will be cleared.
-             */
-            drawStuff();
         }
-        resizeCanvas();
 
-        function drawStuff() {
+
+        function restoreCanvas() {
+            resetCanvas();
+            if (localStorage.getItem('snapshot')) {
+                snapshot = localStorage.getItem('snapshot');
+                drawDataURIOnCanvas(snapshot, canvas);
+            }
+            console.log(snapshot);
+        }
+
+        restoreCanvas();
+        canvas.focus();
+
+        function saveCanvas() {
+            snapshot = canvas.toDataURL('image/jpeg', 1.0);
+            window.localStorage.setItem('snapshot', snapshot);
+        }
+
+        canvas.addEventListener('mousemove', yellowCircle);
+        canvas.addEventListener('keydown', keyDownHandler, true);
+
+        function yellowCircle(e) {
+            console.log('Yellow circle, ');
+
+            ctx.beginPath();
+            ctx.fillStyle = 'yellow';
+            ctx.arc(e.clientX, e.clientY, 50, 0, 2 * Math.PI);
+            ctx.fill();
+            saveCanvas();
+        }
+
+        function keyDownHandler(e) {
+            console.log(e.keyCode);
+
+            if (e.keyCode === 32) {
+                ctx.fillStyle = "black";
+                ctx.fillRect(100, 100, 200, 200);
+                console.log(canvas.toDataURL('image/jpeg', 1.0));
+                saveCanvas();
+            }
+        }
+
+        function keyUpHandler() {
+
+        }
+
+
+
+        function drawStuff(snapshot) {
             // draw stuff
+            // ctx.drawImage(snapshotImage, 0, 0);
+            // snapshotImage.src = snapshot;
         }
 
     } else {
         // canvas-unsupported code here
     }
 
-
+    function drawDataURIOnCanvas(strDataURI, canvas) {
+        "use strict";
+        var img = new window.Image();
+        img.addEventListener("load", function () {
+            canvas.getContext("2d").drawImage(img, 0, 0);
+        });
+        img.setAttribute("src", strDataURI);
+    }
 
 })();
